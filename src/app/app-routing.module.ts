4a8a29fae746a3f2b00f42afa7a14a5ab56765a1
carlosruizr3 <1902392@utrivieramaya.edu.mx>
2023-05-03 21:01:25 -0500

    Se agregaron componentes
    Componentes login y home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    canActivate: [],
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  
+  {
+    path: 'login',
+    canActivate: [],
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
